109d1f3ea778b77d151b57517976c55930854e87
Marcel <Marcel.Voigt92@gmx.de>
2023-03-09 18:38:04 +0100

    finished about. working on expirience

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        As a full stack developer, I possess a broad range of technical skills
+        and experience that enable me to develop and maintain complex web
+        applications from the front-end to the back-end. With expertise in
+        programming languages such as HTML, CSS, JavaScript, and various
+        frameworks like React, Angular and Node.js, I am capable of creating
+        dynamic user interfaces, implementing APIs, managing databases, and
+        ensuring optimal performance and security of web applications. I have a
+        deep understanding of web development principles and practices and am
+        always up to date with the latest technologies and trends in the field.
+        I am also highly proficient in project management, communication, and
+        problem-solving, allowing me to deliver high-quality software solutions
+        that meet the needs of clients and end-users alike.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
