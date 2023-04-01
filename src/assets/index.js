f0508e7019ca3b23d0f1c5f3b4e8a86bf792adba
Marcel <Marcel.Voigt92@gmx.de>
2023-04-01 22:24:58 +0200

    adjustest asses exports

--- src/assets/index.js
+++ src/assets/index.js
@@ -30,7 +30,7 @@ import hcl from "./company/HCL.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import tripguide from "./tripguidee.png";
 import discord from "./discord.png";
 import hoobank from "./hoobank.png";
 import weather from "./weather.png";
